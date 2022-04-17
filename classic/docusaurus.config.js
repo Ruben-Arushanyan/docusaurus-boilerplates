104bf141868f866f1cf06c9931782dba89296f65
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-04-17 21:39:02 +0400

    improvements

--- classic/docusaurus.config.js
+++ classic/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Lox Lava',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
--- classic/src/pages/index.js
+++ classic/src/pages/index.js
@@ -26,11 +26,11 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title=""
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
