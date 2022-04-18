a80f4f9528ac6d158579cd76c3e64eaaddf9865d
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-04-18 21:52:53 +0400

    improvements

--- classic/docusaurus.config.js
+++ classic/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Lox Lava',
   tagline: 'Psakecin Maroin Tvin Choban Karoin',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://lox-lava.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
