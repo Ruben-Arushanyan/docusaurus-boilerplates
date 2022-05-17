ae5e4108ec01419fd7ef8b60dace7ba4e5885f36
Ruben Arushanyan <ruben.arushanyan@fluxtechs.com>
2022-05-17 09:59:47 +0400

    .

--- classic/src/components/HomepageHeader/index.js
+++ classic/src/components/HomepageHeader/index.js
@@ -13,10 +13,16 @@ export default function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
             <Link
-                className="button button--secondary button--lg"
+                className="button button--primary button--lg"
                 to="/docs/intro"
             >
                 Get Started
+            </Link>
+            <Link
+                className="button button--info button--lg"
+                to="/docs/intro"
+            >
+                Try a Demo
             </Link>
         </div>
       </div>
