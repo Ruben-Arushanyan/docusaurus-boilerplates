c362ce7af1f18f8ecc35d1cb0faa8306509ffe12
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-06-19 22:39:52 +0400

    .

--- classic/src/components/HeroBanner/index.js
+++ classic/src/components/HeroBanner/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
@@ -7,6 +6,10 @@ import styles from './index.module.css';
 
 export default function HeroBanner() {
   const {siteConfig} = useDocusaurusContext();
+  const {
+    organizationName,
+    projectName,
+  } = siteConfig;
 
   return (
     <div className={styles.hero} data-theme="dark">
@@ -31,6 +34,15 @@ export default function HeroBanner() {
           <Link className="button button--info" to="/docs/intro">
             Try a Demo
           </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <iframe
+              className={styles.indexCtasGitHubButton}
+              src={`https://ghbtns.com/github-btn.html?user=${organizationName}&repo=${projectName}&type=star&count=true&size=large`}
+              width={160}
+              height={30}
+              title="GitHub Stars"
+            />
+          </span>
         </div>
       </div>
     </div>
